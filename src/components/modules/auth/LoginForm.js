16cd97c8b26004dea4a23f84a5f669a68a168f39
hoanglong040800 <hoanglong040800@gmail.com>
2021-08-03 12:06:45 +0700

    auth input handler

--- src/components/modules/auth/LoginForm.js
+++ src/components/modules/auth/LoginForm.js
@@ -1,21 +1,37 @@
-import {
-  Button,
-  TextField,
-  Box,
-  FormControl,
-  Typography,
-} from '@material-ui/core'
+import { useState } from 'react'
+import { Button, TextField, Box, FormControl, Typography } from '@material-ui/core'
 
 export default function LoginForm() {
+  const [input, setInput] = useState({
+    email: '',
+    password: '',
+  })
+
+  function changeHandler(e) {
+    const name = e.target.name
+    const value = e.target.value
+    setInput({ ...input, [name]: value })
+  }
+
   return (
     <>
       <form noValidate>
         <FormControl margin="dense" fullWidth>
-          <TextField label="Email" name="email" />
+          <TextField
+            label="Email"
+            name="email"
+            value={input.email}
+            onChange={changeHandler}
+          />
         </FormControl>
 
         <FormControl margin="dense" fullWidth>
-          <TextField label="Password" name="password" />
+          <TextField
+            label="Password"
+            name="password"
+            value={input.password}
+            onChange={changeHandler}
+          />
         </FormControl>
 
         <Box textAlign="right" mt={2}>
@@ -26,7 +42,7 @@ export default function LoginForm() {
 
         <Box display="flex" justifyContent="center" mt={4}>
           <Button variant="contained" color="primary">
-            Login
+            Signup
           </Button>
         </Box>
       </form>
--- src/components/modules/auth/SignupForm.js
+++ src/components/modules/auth/SignupForm.js
@@ -1,19 +1,47 @@
 import { Button, TextField, Box, FormControl } from '@material-ui/core'
+import { useState } from 'react'
 
 export default function SignupForm() {
+  const [input, setInput] = useState({
+    email: '',
+    password: '',
+    confirmPassword: '',
+  })
+
+  function changeHandler(e) {
+    const name = e.target.name
+    const value = e.target.value
+    setInput({ ...input, [name]: value })
+  }
+
   return (
     <>
       <form noValidate>
         <FormControl margin="dense" fullWidth>
-          <TextField label="Email" name="email" />
+          <TextField
+            label="Email"
+            name="email"
+            value={input.email}
+            onChange={changeHandler}
+          />
         </FormControl>
 
         <FormControl margin="dense" fullWidth>
-          <TextField label="Password" name="password" />
+          <TextField
+            label="Password"
+            name="password"
+            value={input.password}
+            onChange={changeHandler}
+          />
         </FormControl>
 
         <FormControl margin="dense" fullWidth>
-          <TextField label="Confirm Password" name="confirmPassword" />
+          <TextField
+            label="Confirm Password"
+            name="confirmPassword"
+            value={input.confirmPassword}
+            onChange={changeHandler}
+          />
         </FormControl>
 
         <Box display="flex" justifyContent="center" mt={4}>
