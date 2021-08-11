999ee649dfacabd1dd2caa4cc216816c5ce0eddf
hoanglong040800 <hoanglong040800@gmail.com>
2021-08-11 20:50:27 +0700

    finish UI ItemMentor

--- src/components/modules/mentor/ItemMentor.js
+++ src/components/modules/mentor/ItemMentor.js
@@ -1,33 +1,136 @@
-import Image from 'next/image'
-import { Paper, Box } from '@material-ui/core'
+import { Paper, Box, Button, IconButton, Typography } from '@material-ui/core'
+import {
+  AttachMoney,
+  Code,
+  Favorite,
+  FavoriteBorder,
+  WorkOutlineOutlined,
+} from '@material-ui/icons'
+import { useState } from 'react'
 import classes from 'styles/mentor/ItemMentor.module.css'
 
+const dummyField = [
+  'AI',
+  'Blockchain',
+  'IoT',
+  'AI',
+  'Blockchain',
+  'IoT',
+  'AI',
+  'Blockchain',
+  'IoT',
+]
+
+const dummyTech = [
+  'ReactJS',
+  'NextJS',
+  'Material UI',
+  'BS4',
+  'ReactJS',
+  'NextJS',
+  'Material UI',
+  'BS4',
+]
+
 export default function ItemMentor() {
+  const [fav, setFav] = useState(false)
+
   return (
-    <Paper className={classes.paper}>
-      <div className={classes.imageContainer}>
+    <Paper
+      className={classes.paper}
+      onClick={() => alert('Item Mentor Clicked')}
+    >
+      <div className={classes.thumnail}>
         <img src="https://www.adobe.com/content/dam/cc/us/en/creativecloud/photography/discover/portrait-photography/CODERED_B1_portrait_photography-P4a_438x447.jpg.img.jpg" />
       </div>
 
-      <div className={classes.content}>
-        <Box mt={2}>
-          <h4>Full Name</h4>
-        </Box>
+      <content className={classes.content}>
+        <div className={classes.info}>
+          <Box
+            mb={2}
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <h4>Elon Musk</h4>
 
-        <Box mt={2}>
-          <p>Fields</p>
-        </Box>
+            {fav ? (
+              <IconButton
+                size="small"
+                color="primary"
+                onClick={e => {
+                  e.stopPropagation()
+                  setFav(false)
+                }}
+              >
+                <Favorite />
+              </IconButton>
+            ) : (
+              <IconButton
+                size="small"
+                onClick={e => {
+                  e.stopPropagation()
+                  setFav(true)
+                }}
+              >
+                <FavoriteBorder />
+              </IconButton>
+            )}
+          </Box>
 
-        <Box mt={2}>
-          <p>Language</p>
-        </Box>
+          <Box className={classes.tech}>
+            <WorkOutlineOutlined className={classes.icon} />
 
-        <Box display="flex" justifyContent="space-between" mt={2}>
-          <Box>Mentored: 20</Box>
+            <Box>
+              {dummyField.slice(0, 5).map(item => {
+                return (
+                  <Button
+                    size="small"
+                    className={classes.techItem}
+                    onClick={e => {
+                      e.stopPropagation()
+                    }}
+                  >
+                    {item}
+                  </Button>
+                )
+              })}
+            </Box>
+          </Box>
 
-          <Box>Start at: 20k</Box>
+          <Box className={classes.tech}>
+            <Code className={classes.icon} />
+
+            <Box>
+              {dummyTech.slice(0, 5).map(item => {
+                return (
+                  <Button
+                    size="small"
+                    className={classes.techItem}
+                    onClick={e => {
+                      e.stopPropagation()
+                    }}
+                  >
+                    {item}
+                  </Button>
+                )
+              })}
+            </Box>
+          </Box>
+        </div>
+
+        <Box className={classes.stats}>
+          <p>Mentee: 20</p>
+
+          <Box display="flex" alignItems="center">
+            <AttachMoney size="small" color="secondary" />
+
+            <Typography color="secondary">
+              <b>20k</b>
+            </Typography>
+          </Box>
         </Box>
-      </div>
+      </content>
     </Paper>
   )
 }
--- src/components/modules/mentor/ListMentor.js
+++ src/components/modules/mentor/ListMentor.js
@@ -3,21 +3,21 @@ import ItemMentor from './ItemMentor'
 
 export default function ListMentor() {
   return (
-    <Box bgcolor="#e6e3e3">
+    <Box>
       <Grid container spacing={3}>
-        <Grid item xs={6} sm={4}>
+        <Grid item xs={12} sm={6}>
           <ItemMentor />
         </Grid>
 
-        <Grid item xs={6} sm={4}>
+        <Grid item xs={12} sm={6}>
           <ItemMentor />
         </Grid>
 
-        <Grid item xs={6} sm={4}>
+        <Grid item xs={12} sm={6}>
           <ItemMentor />
         </Grid>
 
-        <Grid item xs={6} sm={4}>
+        <Grid item xs={12} sm={6}>
           <ItemMentor />
         </Grid>
       </Grid>
