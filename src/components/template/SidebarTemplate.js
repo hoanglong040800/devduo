c90347d8313ff9497248abe3d617785f6819ca2f
hoanglong040800 <hoanglong040800@gmail.com>
2021-08-13 13:29:14 +0700

    fix bug toggle filter

--- src/components/template/SidebarTemplate.js
+++ src/components/template/SidebarTemplate.js
@@ -1,23 +1,15 @@
-import { Grid, Paper } from '@material-ui/core'
+import { Box, Grid, Paper } from '@material-ui/core'
 
 export default function SidebarTemplate({ sidebar, main, showSidebar = true }) {
   return (
     <Grid container spacing={2}>
-      {showSidebar ? (
-        <>
-          <Grid item xs={12} md={3}>
-            {sidebar}
-          </Grid>
+      <Grid item xs={12} md={3}>
+        <Box display={showSidebar ? 'block' : 'none'}>{sidebar}</Box>
+      </Grid>
 
-          <Grid item xs={12} md={9}>
-            {main}
-          </Grid>
-        </>
-      ) : (
-        <Grid item xs={12}>
-          {main}
-        </Grid>
-      )}
+      <Grid item xs={12} md={showSidebar ? 9 : 12}>
+        {main}
+      </Grid>
     </Grid>
   )
 }
